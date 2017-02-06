08361d26e9e62d96248310a2716afc56ee31a292
christian99salerno <christian.salerno05@gmail.com>
2017-02-06 17:48:17 +0100

    file da controllare

--- giocatore.py
+++ giocatore.py
@@ -17,16 +17,7 @@ class Giocatore:
         return self.name
     def danno_colpo(self):
         if self.life<=0:
-            print "Il giocatore",self.getName()," e' GAME OVER"
+            return -1
         else:
             self.p.spara() 
             self.life=self.life-10
-         
-#g=Giocatore("chrgianvinc", 10)
-#print g.getLife() 
-#g.danno_colpo()
-#print g.getLife()
-
-#if __name__=='__main__':
-   # g = Giocatore("chris", 100)
-   # print g
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from giocatore import Giocatore
 
 chris = Giocatore("chris", 100)
+
+while(chris.getLife>0):
+    chris.danno_colpo()
+    print chris.getLife()
+
